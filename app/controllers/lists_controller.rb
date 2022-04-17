80af9fe5fdb098b87d8c30d7c6ea4fa3735d7b88
2in2in <88322988+2in2in@users.noreply.github.com>
2022-04-17 13:47:39 +0000

    [finish]chapter8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,17 +14,32 @@ class ListsController < ApplicationController
   end
   
   def create
-    list = List.new(list_params)
-    list.save
-    # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)  
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end  
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
   
-   private
-  # ストロングパラメータ
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+  
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+  end
+  
+  private
   def list_params
-    params.require(:list).permit(:title, :body)
-  end 
+    params.require(:list).permit(:title, :body, :image)  
+  end
 end
